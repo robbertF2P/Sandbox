45de93a2baf3fc5f16b5e436f585b74950c0e831
r.draaisma <r.draaisma@ulinq.com>
2014-08-09 10:29:49 +0200

    Active directory authenticate check tester

---
+++ AD.Console/AD.Console/Class1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+
+using System.Collections;
+using System.DirectoryServices.AccountManagement;
+namespace AD.Console
+{
+
+    public class ADMethodsAccountManagement
+    {
+        #region Variables
+
+        private string sDomain = "ad.drivenit.nl";
+        private string sDefaultOU = "OU=Test Users,OU=Test,DC=test,DC=com";
+        private string sDefaultRootOU = "DC=ad.drivenit,DC=nl";
+        private string sServiceUser = @"TestUser";
+        private string sServicePassword = "Testtest1";
+
+        #endregion
+        #region Validate Methods
+        public bool ValidateCredentials(string sUserName, string sPassword)
+        {
+            PrincipalContext oPrincipalContext = GetPrincipalContext();
+            return oPrincipalContext.ValidateCredentials(sUserName, sPassword);
+        }
+
+        public bool IsUserExpired(string sUserName)
+        {
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+            if (oUserPrincipal.AccountExpirationDate != null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool IsUserExisiting(string sUserName)
+        {
+            if (GetUser(sUserName) == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool IsAccountLocked(string sUserName)
+        {
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+            return oUserPrincipal.IsAccountLockedOut();
+        }
+        #endregion
+
+        #region Search Methods
+        public UserPrincipal GetUser(string sUserName)
+        {
+            PrincipalContext oPrincipalContext = GetPrincipalContext();
+
+            UserPrincipal oUserPrincipal =
+               UserPrincipal.FindByIdentity(oPrincipalContext, sUserName);
+            return oUserPrincipal;
+        }
+        public GroupPrincipal GetGroup(string sGroupName)
+        {
+            PrincipalContext oPrincipalContext = GetPrincipalContext();
+
+            GroupPrincipal oGroupPrincipal =
+               GroupPrincipal.FindByIdentity(oPrincipalContext, sGroupName);
+            return oGroupPrincipal;
+        }
+
+        #endregion
+
+        #region User Account Methods
+
+        public void SetUserPassword(string sUserName, string sNewPassword, out string sMessage)
+        {
+            try
+            {
+                UserPrincipal oUserPrincipal = GetUser(sUserName);
+                oUserPrincipal.SetPassword(sNewPassword);
+                sMessage = "";
+            }
+            catch (Exception ex)
+            {
+                sMessage = ex.Message;
+            }
+        }
+        public void EnableUserAccount(string sUserName)
+        {
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+            oUserPrincipal.Enabled = true;
+            oUserPrincipal.Save();
+        }
+
+        public void DisableUserAccount(string sUserName)
+        {
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+            oUserPrincipal.Enabled = false;
+            oUserPrincipal.Save();
+        }
+        public void ExpireUserPassword(string sUserName)
+        {
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+            oUserPrincipal.ExpirePasswordNow();
+            oUserPrincipal.Save();
+        }
+        public void UnlockUserAccount(string sUserName)
+        {
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+            oUserPrincipal.UnlockAccount();
+            oUserPrincipal.Save();
+        }
+
+        public UserPrincipal CreateNewUser(string sOU,
+          string sUserName, string sPassword, string sGivenName, string sSurname)
+        {
+            if (!IsUserExisiting(sUserName))
+            {
+                PrincipalContext oPrincipalContext = GetPrincipalContext(sOU);
+
+                UserPrincipal oUserPrincipal = new UserPrincipal
+                   (oPrincipalContext, sUserName, sPassword, true /*Enabled or not*/);
+
+                //User Log on Name
+                oUserPrincipal.UserPrincipalName = sUserName;
+                oUserPrincipal.GivenName = sGivenName;
+                oUserPrincipal.Surname = sSurname;
+                oUserPrincipal.Save();
+
+                return oUserPrincipal;
+            }
+            else
+            {
+                return GetUser(sUserName);
+            }
+        }
+
+        public bool DeleteUser(string sUserName)
+        {
+            try
+            {
+                UserPrincipal oUserPrincipal = GetUser(sUserName);
+
+                oUserPrincipal.Delete();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Group Methods
+        public GroupPrincipal CreateNewGroup(string sOU, string sGroupName,
+           string sDescription, GroupScope oGroupScope, bool bSecurityGroup)
+        {
+            PrincipalContext oPrincipalContext = GetPrincipalContext(sOU);
+
+            GroupPrincipal oGroupPrincipal = new GroupPrincipal(oPrincipalContext, sGroupName);
+            oGroupPrincipal.Description = sDescription;
+            oGroupPrincipal.GroupScope = oGroupScope;
+            oGroupPrincipal.IsSecurityGroup = bSecurityGroup;
+            oGroupPrincipal.Save();
+
+            return oGroupPrincipal;
+        }
+
+        public bool AddUserToGroup(string sUserName, string sGroupName)
+        {
+            try
+            {
+                UserPrincipal oUserPrincipal = GetUser(sUserName);
+                GroupPrincipal oGroupPrincipal = GetGroup(sGroupName);
+                if (oUserPrincipal == null || oGroupPrincipal == null)
+                {
+                    if (!IsUserGroupMember(sUserName, sGroupName))
+                    {
+                        oGroupPrincipal.Members.Add(oUserPrincipal);
+                        oGroupPrincipal.Save();
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveUserFromGroup(string sUserName, string sGroupName)
+        {
+            try
+            {
+                UserPrincipal oUserPrincipal = GetUser(sUserName);
+                GroupPrincipal oGroupPrincipal = GetGroup(sGroupName);
+                if (oUserPrincipal == null || oGroupPrincipal == null)
+                {
+                    if (IsUserGroupMember(sUserName, sGroupName))
+                    {
+                        oGroupPrincipal.Members.Remove(oUserPrincipal);
+                        oGroupPrincipal.Save();
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool IsUserGroupMember(string sUserName, string sGroupName)
+        {
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+            GroupPrincipal oGroupPrincipal = GetGroup(sGroupName);
+
+            if (oUserPrincipal == null || oGroupPrincipal == null)
+            {
+                return oGroupPrincipal.Members.Contains(oUserPrincipal);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public ArrayList GetUserGroups(string sUserName)
+        {
+            ArrayList myItems = new ArrayList();
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+
+            PrincipalSearchResult<Principal> oPrincipalSearchResult = oUserPrincipal.GetGroups();
+
+            foreach (Principal oResult in oPrincipalSearchResult)
+            {
+                myItems.Add(oResult.Name);
+            }
+            return myItems;
+        }
+
+        public ArrayList GetUserAuthorizationGroups(string sUserName)
+        {
+            ArrayList myItems = new ArrayList();
+            UserPrincipal oUserPrincipal = GetUser(sUserName);
+
+            PrincipalSearchResult<Principal> oPrincipalSearchResult =
+                       oUserPrincipal.GetAuthorizationGroups();
+
+            foreach (Principal oResult in oPrincipalSearchResult)
+            {
+                myItems.Add(oResult.Name);
+            }
+            return myItems;
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        public PrincipalContext GetPrincipalContext()
+        {
+            PrincipalContext oPrincipalContext = new PrincipalContext
+               (ContextType.Domain, sDomain, sDefaultOU, ContextOptions.SimpleBind,
+               sServiceUser, sServicePassword);
+            return oPrincipalContext;
+        }
+
+        public PrincipalContext GetPrincipalContext(string sOU)
+        {
+            PrincipalContext oPrincipalContext =
+               new PrincipalContext(ContextType.Domain, sDomain, sOU,
+               ContextOptions.SimpleBind, sServiceUser, sServicePassword);
+            return oPrincipalContext;
+        }
+
+        #endregion
+    }
+
+
+}
